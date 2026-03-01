fbd0a6614ebfbb6289a1352f067b540d329216e4
EHadoux <EHadoux@users.noreply.github.com>
2026-03-01 17:27:04 +0000

    deploy: 0650069fb9133a33ade11d698a7451aa4b2ad3c6

---
+++ _app/immutable/chunks/BshY1QPt.js
@@ -0,0 +1,4 @@
+const e={date:"2024-10-15",draft:!1,title:"Améliorer et garantir la santé des femmes tout au long de leur vie - N° 445",auteurs:["Prisca Thevenot","Julie Delpech","Virginie Duby-Muller","Agnès Firmin Le Bodo","Guillaume Gouffier Valente","Sandrine Josso","Delphine Lingemann","Véronique Riotton"],link:"https://www.assemblee-nationale.fr/dyn/17/dossiers/ameliorer_garantir_sante_femmes",stepsName:["Dépôt à l'Assemblée nationale","Renvoi en commission"],stepsDate:["Mardi 15 octobre 2024","Mardi 15 octobre 2024"],stepsStatus:["","Commission des affaires sociales"],tags:["Relations parlementaires"]},s=`<h2>Résumé</h2>
+<p>L&#39;exposé des motifs n&#39;est pas fourni dans l&#39;extrait de la proposition de loi. Il n&#39;est donc pas possible de le résumer.</p>
+<h2>Liste des modifications</h2>
+`;export{s as default,e as metadata};
--- _app/immutable/entry/start.2DiZ0rGY.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BH6y_L49.js";export{o as load_css,r as start};
