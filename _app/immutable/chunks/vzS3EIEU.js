a1e870da7075393625f104b990d85a1aae740f5c
EHadoux <EHadoux@users.noreply.github.com>
2026-04-17 08:06:46 +0000

    deploy: 9955181dfb67cf1ec48ef7e91981deabf320103a

---
+++ _app/immutable/chunks/vzS3EIEU.js
@@ -0,0 +1,5 @@
+const e={date:"2026-04-16",draft:!1,title:"Mettre fin au devoir conjugal - N° 2645",auteurs:["M. Le Président du Sénat"],link:"https://www.assemblee-nationale.fr/dyn/17/dossiers/_mettre_fin_au_devoir_conjugual_17e",stepsName:["Dépôt à l'Assemblée nationale","Renvoi en commission","Première lecture à l'Assemblée nationale","Renvoi en commission","Première lecture au Sénat","Renvoi en commission"],stepsDate:["Mardi 2 décembre 2025","Mardi 2 décembre 2025","Mardi 2 décembre 2025","Jeudi 29 janvier 2026","Jeudi 29 janvier 2026","Mardi 14 avril 2026"],stepsStatus:["","Commission des lois","Texte adopté ✔️","Commission des lois (Sénat)","Texte modifié","Commission des lois"],tags:["Justice","Droits des femmes"]},i=`<h2>Résumé</h2>
+<p>Cette proposition de loi vise à supprimer juridiquement la notion de devoir conjugal afin de renforcer le respect du consentement au sein du couple.</p>
+<h2>Liste des modifications</h2>
+<p><strong>Suppression de la mesure (Article 2)</strong>: L&#39;article a été supprimé par le Sénat lors de l&#39;examen du texte.</p>
+`;export{i as default,e as metadata};
